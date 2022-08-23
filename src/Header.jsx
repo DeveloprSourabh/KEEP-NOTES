2117abc9ed66ef408acd0c504d7f1c9322e8c813
Sourabh <developr.sourabh@gmail.com>
2022-08-23 15:02:07 +0530

    add header and styling

---
+++ src/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+const Header = () => {
+  return (
+    <>
+      <div className="header">
+        <img
+          className="img1"
+          src="https://play-lh.googleusercontent.com/YsyGTAx0w5vlzSE_l3gF_Nm8hKyxw8wSjtbKOmcm-elPLuCOlR-D5wBucNUQup6FCTQ"
+          alt="logo"
+        />
+        <h1 className="he1">Notes Keeper</h1>
+      </div>
+    </>
+  );
+};
+
+export default Header;
