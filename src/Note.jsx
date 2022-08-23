2cb1a94e4928de3a1d3f2e7a5258c043c7fb8add
Sourabh <developr.sourabh@gmail.com>
2022-08-23 16:31:36 +0530

    add new input and icon

--- src/Note.jsx
+++ src/Note.jsx
@@ -1,9 +1,16 @@
+import React from "react";
+import "./App.css";
+
 const Note = () => {
   return (
     <>
       <div className="note">
-        <h1>Title</h1>
+        <h4 className="p2">Title</h4>
         <br />
+        <p className="p2">this is the content</p>
+        <button className="b1">
+          <i className="material-icons ic1">delete</i>
+        </button>
       </div>
     </>
   );
