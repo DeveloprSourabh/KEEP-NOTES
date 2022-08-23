dd0759c0c34d29de0dd85ca36fe4859dfaa5d0e9
Sourabh <developr.sourabh@gmail.com>
2022-08-23 15:18:42 +0530

    add header and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
-
+import Header from "./Header";
+import Footer from "./Footer";
 const App = () => {
-  return <>
-  
-  </>;
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
 };
 
 export default App;
